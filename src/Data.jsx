d36ca913cccebe5d008e412bcd1a2179f3f0b7b4
bim121 <vitisik54@gmail.com>
2023-07-24 22:19:32 +0300

    init

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "yousaf khan",
+        email: "yousaf1@gmail.com",
+        id: 1
+    },
+    {
+        name: "khaidl",
+        email: "khaidl@gmail.com",
+        id: 2
+    },
+    {
+        name: "Muhamed Yousaf",
+        email: "ypusfa@gmail.com",
+        id: 3
+    }
+]
